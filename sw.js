dd8ad8f1173e18048655a49d04b2179a26ecb72b
titangym07142026-max <titangym07142026@gmail.com>
2026-09-22 10:59:04 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,1 @@
-const CACHE='titan-gym-v1';
-const CORE=['./','./index.html','./manifest.webmanifest','./sw.js'];
-self.addEventListener('install',e=>{
- e.waitUntil(caches.open(CACHE).then(async c=>{
-   await c.addAll(CORE);
-   try{await c.add('https://cdnjs.cloudflare.com/ajax/libs/qrcode-generator/1.4.4/qrcode.min.js')}catch(e){}
- }).then(()=>self.skipWaiting()));
-});
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
-   const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return res;
- }).catch(()=>caches.match('./index.html'))));
-});
+const C='titan-entrance-qr-v2',A=['./','./index.html','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(async c=>{await c.addAll(A);try{await c.add('https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js')}catch(e){}try{await c.add('https://cdnjs.cloudflare.com/ajax/libs/qrcode-generator/1.4.4/qrcode.min.js')}catch(e){}return self.skipWaiting()})));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{let y=x.clone();caches.open(C).then(c=>c.put(e.request,y));return x}).catch(()=>caches.match('./index.html')))));
